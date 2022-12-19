2882e68464119428b0e36c4b7e9f2ac509c9f797
juju <julien.adami@etu.iut-tlse3.fr>
2022-12-19 20:01:31 +0100

    disabled main

--- src/main/java/testplug/testplug/Main.java
+++ src/main/java/testplug/testplug/Main.java
@@ -15,5 +15,6 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info(ChatColor.GREEN + "Disabled " + this.getName());
     }
 }
